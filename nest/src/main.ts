f8fb5350d896fcd32f6d970fa9742555ae71d386
rian <rianrgomes@gmail.com>
2024-01-12 18:15:59 -0300

    cors change

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -7,10 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: 'http://localhost:4200',
-  //   credentials: true
-  // })
+  app.enableCors({
+    origin: '*',
+    credentials: true
+  })
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
